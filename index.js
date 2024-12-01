d9474d1b3f2f329a315ac2590d50758537477cbd
Nkateko Nkuna <tekon@Binary-NN.vodacom.corp>
2024-12-01 12:29:56 +0200

    icons

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ const app = express();
 const PORT = process.env.PORT || 2001;
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname,'public')));
 
 
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'public','index.html'))
+    res.sendFile(path.join(__dirname,'public','index.html'));
 
 });
+
 
 app.listen(PORT,() =>{
     console.log(`Server started on port ${PORT}`);
